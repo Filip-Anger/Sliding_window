7c41fc28ef023fb9e6c01408be3cff42bd03a8d9
FilipAnger <filip.anger@gmail.com>
2026-02-26 22:02:49 +0100

    better dataset with more incidents needed

--- window1.py
+++ window1.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 with open("dataset/ec2_cpu_utilization_53ea38.csv") as f:
     series = pd.read_csv(f)
@@ -8,23 +9,19 @@ incidents = ["2014-02-19 19:10:00", "2014-02-23 20:05:00"]
 incident_windows = ["2014-02-19 10:50:00.000000", "2014-02-20 03:30:00.000000"]
 
 series['is_incident'] = series["timestamp"].isin(incidents).astype(int)
-print(series)
 
 w_size = 5
-window = series[0: w_size]
 h_size = 3
-prediction = series[w_size: w_size + h_size]
-present_incidents = prediction['is_incident'].sum()
+X = []
+y = []
+# Pandas magic making lables
+series['target'] = series['is_incident'].rolling(window=h_size).sum().shift(-h_size)
 
 for i in range(w_size, len(series) - h_size):
-    labels[window] = present_incidents
+    # 1. Grab the window of 'value' from i-W to i
+    window = series['value'].iloc[i-w_size : i].values
+    X.append(window)
     
-    window.popleft()
-    window.append(series[i])
-    
-    poped = prediction.popleft()
-    if poped['is_incident'] == 1:
-        present_incidents -= 1
-    prediction.append(series[h_size + i])
-    if series[h_size + i]['is_incident'] == 1:
-        present_incidents += 1
+    # 2. Grab the target we calculated for this point
+    label = series['target'].iloc[i]
+    y.append(label)
